53b1e8e47da888b44b1611dde5628ee267254e4c
JP Montagnet <x-github@jope.net>
2022-01-23 18:15:29 -0800

    Fixed up test_misc.py. All tests now pass, except for generated input/output... because no generated input has been generated yet. Created stubs for those two files though.

--- test_misc.py
+++ test_misc.py
@@ -3,32 +3,33 @@ from io import StringIO
 from minemapper import xlate_grids
 
 
-def test_validate_program_1_field():
+def test_xlate_1_field():
     test_infile = StringIO(initial_value="""
 2 2
 ..
 ..
 0 0
-""")
-    output = StringIO
-    expected_outfile = """
+""".lstrip())
+    output = StringIO()
+    expected_output = """
 Field #1:
 00
 00
+
 """.lstrip()
     xlate_grids(test_infile, output)
-    assert output == expected_outfile
+    assert output.getvalue() == expected_output
 
 
-def test_validate_program_0_field():
-    test_infile = StringIO(initial_value='0 0')
-    output = StringIO
-    expected_outfile = ''.lstrip()
+def test_xlate_0_field():
+    test_infile = StringIO(initial_value='0 0\n')
+    output = StringIO()
+    expected_output = ''
     xlate_grids(test_infile, output)
-    assert output == expected_outfile
+    assert output.getvalue() == expected_output
 
 
-def test_validate_program_3_fields():
+def test_xlate_3_fields():
     test_infile = StringIO(initial_value="""
 1 2
 ..
@@ -40,27 +41,38 @@ def test_validate_program_3_fields():
 ..*..
 .....
 0 0
-""")
-    output = StringIO
-    expected_outfile = """
+""".lstrip())
+    output = StringIO()
+    expected_output = """
 Field #1:
 00
 
 Field #2:
 2*1
-*20
+*21
 
 Field #3:
 *212*
 12*21
 01110
+
 """.lstrip()
     xlate_grids(test_infile, output)
-    assert output == expected_outfile
+    assert output.getvalue() == expected_output
 
 
-def test_validate_program_entire_input_file():
-    with open('minesweeper_input.txt', 'r') as infile, open('minesweeper_output.txt') as outfile, \
-    open('minesweeper_valid_output.txt') as expected_outfile:
-        xlate_grids(infile, outfile)
-        assert outfile == expected_outfile
+def _test_xlate_files(infile_name: str, outfile_name: str):
+    with open(infile_name, 'r') as infile, StringIO() as output, \
+            open(outfile_name, 'r') as expected_outfile:
+        xlate_grids(infile, output)
+        assert output.getvalue() == expected_outfile.read()
+
+
+def test_xlate_official():
+    _test_xlate_files(infile_name='official_input.txt', outfile_name='official_output.txt')
+
+
+def test_xlate_generated():
+    _test_xlate_files(infile_name='minesweeper_input.txt', outfile_name='minesweeper_output.txt')
+
+# END
